9ed39a32a467214230e7b02b65c1b090e7fd4c79
Timothy Verhaeghe <timothy@taglayer.com>
2016-06-23 17:30:49 +0200

    new update

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Hotkeys
 Lightweight hotkeys javascript
 
+## The Idea
+- HotkeysHtml (div, set: tags:"" src="")
+
 ## Get started
 ```js
 <script src="hotkeys/hotkeys.js"></script>
+
+<!-- use Javascript -->
 <script type="text/javascript">
 hotkeys("ctrl+u", function(){
   // the user clicked on ctrl + u (source code)
@@ -11,6 +16,15 @@ hotkeys("ctrl+u", function(){
 });
 </script>
 ```
+
+## Predefined functions
+- sourceCode(callback) => if someone opens the sourcecode (in browser)
+
+## Important
+- You cannot define multiple hotkeys-functions with the same hotkeys (only the last one will be activated);
+
+## Todo
+- HotkeysHtml
 
 ## Credits
 Copyright © 2016 Timothy Verhaeghe <<timothy@taglayer.com>>
--- hotkeys/hotkeys.js
+++ hotkeys/hotkeys.js
@@ -2,29 +2,56 @@
 // Source1: http://www.cambiaresearch.com/articles/15/javascript-key-codes
 // Source2  http://www.w3schools.com/charsets/ref_utf_basic_latin.asp
 // keys[keys.length-1]
-var keys = [];
+
+var timeline = [];
+var hotkeysList = [];
+
 var show = function(){
-  console.log(keys);
+  console.log(timeline);
 }
 
-// cntrl + shift + i
+var hotkeys = function(hotkeyData, callback){
+  console.log('Hotkey added '+ hotkeyData);
+  hotkeysList.push(hotkeyData);
+  hotkeysList[hotkeyData] = callback;
+}
+
+var listen = function(){
+  for (var b = 0; b < hotkeysList.length; b++){
+    var key = hotkeysList[b];
+    var combo = key.split('+');
+    var lastKey = timeline.length;
+    var i = combo.length;
+    var combol = combo.length;
+    var o = 0;
+
+    while(i > 0){
+      i--;
+      lastKey--;
+      if(timeline[lastKey] == combo[i]){
+        o++;
+        if(o == combol){
+          hotkeysList[key]();
+        }
+      } else {
+        i = -1;
+      }
+    }
+  }
+}
+
 document.addEventListener("mousedown", function(evt){
   var click = evt.buttons, t = "";
   if(click == 1){ t = 'left'; }
   if(click == 2){ t = 'right'; }
   if(click == 3){ t = 'together'; }
   if(click == 4){ t = 'scrolbar'; }
-  keys.push("mouse_"+t);
-  show();
+  timeline.push("mouse_"+t);
+  listen();
 });
 
 document.onkeydown = function checkKey(e){
   e = e || window.event;
-  keys.push(String.fromCharCode(e.keyCode));
-  show();
-}
-
-
-var hotkeys = function(keys, callback){
-
+  timeline.push(String.fromCharCode(e.keyCode));
+  listen();
 }
